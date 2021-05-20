b7db554ab2ab3d078cfeaed34f3cd510e1bd5c6d
Keyno <kofujiw@gmail.com>
2021-05-20 18:42:59 +0900

    HTTP のハンドラを mux に変更する

--- src/main/webapi/main.go
+++ src/main/webapi/main.go
@@ -16,20 +16,22 @@ type Person struct {
 }
 
 func main() {
-	//mux := http.NewServeMux()
-	//mux.HandleFunc("/", baseHandler)
-	//mux.HandleFunc("/:path", jsonHandler)
-	//http.ListenAndServe("127.0.0.1:3000", mux)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", baseHandler)
+	//mux.HandleFunc("/json", jsonHandler)
+	mux.HandleFunc("/cookie", getCookie)
+	http.ListenAndServe("127.0.0.1:3000", mux)
 
 	value := "value"
 	var xxx *string
 	xxx = &value
 	fmt.Printf("value: %s", *xxx)
 
-	router := httprouter.New()
-	//router.GET("/", baseHandler)
-	router.GET("/sample/:path", jsonHandler)
-	http.ListenAndServe("127.0.0.1:3000", router)
+	//router := httprouter.New()
+	////router.GET("/", baseHandler)
+	//router.GET("/sample/:path", jsonHandler)
+	//router.GET("/value/sample", getHeader)
+	//http.ListenAndServe("127.0.0.1:3000", router)
 }
 
 func baseHandler(w http.ResponseWriter, q *http.Request) {
@@ -43,11 +45,28 @@ func baseHandler(w http.ResponseWriter, q *http.Request) {
 	w.Write(jsonMessage)
 }
 
+func getCookie(w http.ResponseWriter, q *http.Request) {
+	name := "sample"
+	value, err := parseAndGetCookieValue(q, "sample")
+	if err != nil {
+		fmt.Printf("err, err=[%#v]", err)
+		fmt.Fprintf(w, "Hello getCookie, err=[%#v]", err)
+		return
+	}
+	fmt.Fprintf(w, "Hello, key=[%s], value=[%s]", name, value)
+}
+
+// router handler
 func jsonHandler(w http.ResponseWriter, q *http.Request, p httprouter.Params) {
 	fmt.Fprintf(w, "Hello, %q", p.ByName("user"))
 }
 
-func getHeader(w http.ResponseWriter, q *http.Request) {
-	sample, _ := q.Cookie("sample")
-	fmt.Fprintf(w, "Hello, %s", sample)
+// private function
+func parseAndGetCookieValue(q *http.Request, cookieName string) (string, error) {
+	cookie, err := q.Cookie(cookieName)
+	if err != nil {
+		return "", err
+	}
+
+	return cookie.Value, nil
 }
